377a5a555e0f46253b3a5899926316ee38e54770
Rodolphe <ldnpub@hotmail.fr>
2019-05-10 18:00:18 +0200

    setutp.sh test

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 ID="$(whoami)"   # --->  echo "${ID}"
 WORDKING_DIR="$(pwd)"
 DATE="$(date +-%Y%m%d)"
-DICTORY_BACKUP="/home/$ID/.dotfile_backup"
+DIRECTORY_BACKUP="/home/$ID/.dotfile_backup"
 
 # cron jobs might not have /sbin in their path.
 export PATH="$PATH:/sbin"
@@ -12,12 +12,12 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-if [ ! -d "$DICTORY_BACKUP" ]; then
-  mkdir -p $DICTORY_BACKUP
+if [ ! -d "$DIRECTORY_BACKUP" ]; then
+  mkdir -p $DIRECTORY_BACKUP
 fi
 
 SAVE_BASH() {
-    cp /home/${ID}/$1 ${DIRECTORY}/$1.back${DATE}
+    cp /home/${ID}/$1 ${DIRECTORY_BACKUP}/$1.back${DATE}
     ln /home/${ID}/$1 ${WORDKING_DIR}/$1
 }
 
@@ -25,12 +25,12 @@ SAVE_BASH .bashrc
 SAVE_BASH .bash_profile
 SAVE_BASH .bash_prompt
 
-SAVE_XFCE() {
-
-}
-~/.config/Thunar
-~/.local/share/xfce4
-~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+#SAVE_XFCE() {
+#
+#}
+#~/.config/Thunar
+#~/.local/share/xfce4
+#~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
 
 
 
